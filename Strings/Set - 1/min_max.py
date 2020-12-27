cbeb51d4465a67abba82d3b9e2112416e1deb8cf
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2020-12-27 11:42:41 +0530

    Create min_max.py

---
+++ Strings/Set - 1/min_max.py
@@ -0,0 +1,30 @@
+"""
+min(“string”) :- 
+This function returns the minimum value alphabet from string.
+
+max(“string”) :- 
+This function returns the maximum value alphabet from string.
+"""
+
+# Python code to demonstrate working of 
+# min() and max() 
+str = "geeksforgeeks"
+
+# using min() to print the smallest character 
+# prints 'e' 
+print ("The minimum value character is : " + min(str)) 
+
+# using max() to print the largest character 
+# prints 's' 
+print ("The maximum value character is : " + max(str)) 
+
+
+
+"""
+
+Output:
+
+The minimum value character is : e
+The maximum value character is : s
+
+"""
