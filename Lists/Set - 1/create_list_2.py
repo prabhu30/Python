0f8db5f16674b21e43bd396000e3da65f61cbced
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2020-12-27 12:08:44 +0530

    Create create_list_2.py

---
+++ Lists/Set - 1/create_list_2.py
@@ -0,0 +1,31 @@
+"""
+A list may contain duplicate values with their distinct positions and hence, 
+multiple distinct or duplicate values can be passed as a sequence at the time of list creation.
+"""
+
+# Creating a List with 
+# the use of Numbers 
+# (Having duplicate values) 
+List = [1, 2, 4, 4, 3, 3, 3, 6, 5] 
+print("\nList with the use of Numbers: ") 
+print(List) 
+
+# Creating a List with 
+# mixed type of values 
+# (Having numbers and strings) 
+List = [1, 2, 'Geeks', 4, 'For', 6, 'Geeks'] 
+print("\nList with the use of Mixed Values: ") 
+print(List) 
+
+
+
+"""
+
+Output:
+List with the use of Numbers: 
+[1, 2, 4, 4, 3, 3, 3, 6, 5]
+
+List with the use of Mixed Values: 
+[1, 2, 'Geeks', 4, 'For', 6, 'Geeks']
+
+"""
