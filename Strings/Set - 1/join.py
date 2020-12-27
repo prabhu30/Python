72f0108062d7695c8dad859245433a7c32cf96a1
prabhu kalyan <57695741+prabhu30@users.noreply.github.com>
2020-12-27 11:38:44 +0530

    Create join.py

---
+++ Strings/Set - 1/join.py
@@ -0,0 +1,23 @@
+"""
+join() :- 
+This function is used to join a sequence of strings mentioned in its arguments with the string.
+"""
+
+# Python code to demonstrate working of 
+# join() 
+str = "_"
+str1 = ( "geeks", "for", "geeks" ) 
+
+# using join() to join sequence str1 with str 
+print ("The string after joining is : ", end="") 
+print ( str.join(str1)) 
+
+
+
+"""
+
+Output:
+
+The string after joining is : geeks_for_geeks
+
+"""
